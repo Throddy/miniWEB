a211c04b7276574fc0258ab8a44bed51b85bec5a
Kostyan <vitvkvk@gmail.com>
2025-02-21 19:02:42 +0200

    picture creation

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-import requests
 from io import BytesIO
+import requests
 from PIL import Image
-import random
-import time
-import pygame
-
-GEOCODER_API = "8013b162-6b42-4997-9691-77b7074026e0"
-YANDEX_MAPS_API = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
-
-towns_list = ['Киров', 'Москва', 'Цюрих', 'Шадринск', 'Лондон', 'Верне', 'Мараба', 'Росарио', 'Новый Орлеан', 'Кумаси']
-ZOOM = "16"
-SLIDE_DURATION = 2
-WINDOW_SIZE = (450, 450)
 
 
+def ll_cords(toponym_to_find):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+        "geocode": toponym_to_find,
+        "format": "json"}
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+    if not response:
+        pass
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    return toponym_coodrinates.split(" ")[::-1]
 
-def show_pic_cords(cor1, cor2):
-    if cor1 is None or cor2 is None:
-        return None
 
-    ll = '{},{}'.format(cor1, cor2)
+def show_find(adress='', cords1='', cords2=''):
+    if adress:
+        ll = ll_cords(adress)
+        print(ll)
+    if cords1 and cords2:
+        ll = (str(cords1), str(cords2))
+        print(ll)
+    delta = "0.005"
+    apikey = "46f543e5-8557-4b4f-92bc-8fbd63dcd53d"
+    org_point = f'{ll[1]},{ll[0]}'
 
-    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    # Собираем параметры для запроса к StaticMapsAPI:
     map_params = {
-            "ll": ll,
-            "size": "450,450",
-            "l": "map",
-            "z": ZOOM,
-            "apikey": YANDEX_MAPS_API
-        }
-
+        "ll": ",".join([ll[1], ll[0]]),
+        "spn": ",".join([delta, delta]),
+        "apikey": apikey,
+        'pt': f'{org_point},pm2dgl'
+    }
+    print(map_params)
+    map_api_server = "https://static-maps.yandex.ru/v1"
     response = requests.get(map_api_server, params=map_params)
-    if response.status_code != 200:
-        print(f"err while loading map of {response.status_code}: {response.text}")
-        return
+    print(response)
+    im = BytesIO(response.content)
+    opened_image = Image.open(im)
+    opened_image.show()
 
-    img = Image.open(BytesIO(response.content))
-    if is_city_area(img):
-        return BytesIO(response.content)
+
+show_find(adress="Калининград")
